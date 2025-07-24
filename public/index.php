70b9d6140ff2082cfccb94aefc86bcb0024ac5b3
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-07-24 18:47:49 -0300

    Fix autoload path in index.php for correct file inclusion

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-require_once dirname(__DIR__) . '/../autoload.php';
+require_once dirname(__DIR__) . '/autoload.php';
 
 use Config\RenderingConfig;
 use Rendering\Infrastructure\RenderingKernel;
